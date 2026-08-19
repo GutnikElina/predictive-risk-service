5eb8f611e01648665db735be5889f0d942ce7ac3
Antrsss <zgdasha2510@gmail.com>
2026-08-19 14:27:05 +0300

    feat: add unique cypher migration

---
+++ src/main/java/com/innowise/predictiveriskservice/entity/VesselNode.java
@@ -0,0 +1,14 @@
+package com.innowise.predictiveriskservice.entity;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+@Node("Vessel")
+public class VesselNode {
+
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    private String id;
+}
