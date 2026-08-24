3a2727ac1bbdb69660600909234cee9d14587bb5
Antrsss <zgdasha2510@gmail.com>
2026-08-24 10:53:09 +0300

    refactor: change entity id from String to UUID

--- src/main/java/com/innowise/predictiveriskservice/entity/PortNode.java
+++ src/main/java/com/innowise/predictiveriskservice/entity/PortNode.java
@@ -3,12 +3,13 @@ package com.innowise.predictiveriskservice.entity;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import java.util.UUID;
 
 @Node("Port")
 public class PortNode {
 
     @Id
-    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
-    private String id;
+    @GeneratedValue
+    private UUID id;
 }
