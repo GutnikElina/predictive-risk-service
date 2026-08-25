a944d3157832d298ee6d0f7eded56df918d800e3
Antrsss <zgdasha2510@gmail.com>
2026-08-25 11:27:21 +0300

    feat: add LogisticsGraphService

--- src/main/java/com/innowise/predictiveriskservice/service/LogisticsGraphService.java
+++ src/main/java/com/innowise/predictiveriskservice/service/LogisticsGraphService.java
@@ -1,4 +1,35 @@
 package com.innowise.predictiveriskservice.service;
 
+import com.innowise.predictiveriskservice.entity.ContainerNode;
+import com.innowise.predictiveriskservice.entity.ShipmentNode;
+import com.innowise.predictiveriskservice.repository.ContainerRepository;
+import com.innowise.predictiveriskservice.repository.ShipmentOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class LogisticsGraphService {
+
+    private final ContainerRepository containerRepository;
+    private final ShipmentOrderRepository shipmentOrderRepository;
+
+    public Mono<ShipmentNode> linkContainerToShipment(String shipmentId, String containerId) {
+        return shipmentOrderRepository.linkContainerToShipment(
+                UUID.fromString(shipmentId),
+                UUID.fromString(containerId)
+        );
+    }
+
+    public Mono<ContainerNode> linkVesselToContainer(String containerId, String vesselId) {
+        return containerRepository.linkVesselToContainer(
+                UUID.fromString(containerId),
+                UUID.fromString(vesselId)
+        );
+    }
 }
