065511ce6d2ce1607e43f98b3647943a3976e06e
Antrsss <zgdasha2510@gmail.com>
2026-08-25 13:13:17 +0300

    fix: add @RequestMapping

--- src/main/java/com/innowise/predictiveriskservice/controller/LogisticsController.java
+++ src/main/java/com/innowise/predictiveriskservice/controller/LogisticsController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-@RestController("/api/v1/logistics")
+@RestController
+@RequestMapping("/api/v1/logistics")
 @RequiredArgsConstructor
 public class LogisticsController {
 
