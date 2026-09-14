a444a66ed61e7c29ec999201d2ebb132ea070a35
Antrsss <zgdasha2510@gmail.com>
2026-09-14 12:33:49 +0300

    fix: add orderId

--- src/main/java/com/innowise/predictiveriskservice/entity/RiskScoreCalculationData.java
+++ src/main/java/com/innowise/predictiveriskservice/entity/RiskScoreCalculationData.java
@@ -1,6 +1,9 @@
 package com.innowise.predictiveriskservice.entity;
 
+import java.util.UUID;
+
 public record RiskScoreCalculationData(
+        UUID orderId,
         double delay,
         double weather,
         double vendorHistory,
